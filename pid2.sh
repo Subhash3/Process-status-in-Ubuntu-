b843a1073701f4cf39132a06d01dc79003b3ed33
Subhash3 <32383953+Subhash3@users.noreply.github.com>
2017-11-17 07:33:48 +0000

    Add files via upload

---
+++ pid2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Based on ps
+#Syntax: ./pid.sh <pid>
+clear
+if [ $# -ne 1 ]
+then
+        echo -e "\e[1;31m !!Ooppss!! \e[0m--\e[1;33m Bad Syntax\e[0m--"
+        echo "Usage: ./pid.sh <pid>"
+        echo ---------------------------
+        exit 1
+fi
+
+if [ $(ps | awk '{print $1}' | grep -i $1$) ]
+then
+        echo -e "The process is \e[1;33m still running\e[0m"
+else
+        echo -e "The process is \e[1;33m not running\e[0m" 
+fi
+
